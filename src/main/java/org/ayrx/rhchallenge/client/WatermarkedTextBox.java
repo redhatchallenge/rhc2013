6154110ca08da20e873d6b76ef61cdb3405f0ae4
Ayrx <terrycwk1994@gmail.com>
2013-08-13 10:14:06 +0800

    Added a phone number watermarks on the registration pages.
    * New WatermarkedTextBox widget that extends TextBox to support
      this function.
    * Added in different watermarks for each individual country as per
      the phone number format for each country.
    * NOTE: One must apply a cheap hack of calling .setText("") when
      changing the watermarks using event handlers due to some issues
      with the styling. An example of this can be found on RegisterScreen
      line 96.

---
+++ src/main/java/org/ayrx/rhchallenge/client/WatermarkedTextBox.java
@@ -0,0 +1,69 @@
+package org.ayrx.rhchallenge.client;
+
+import com.google.gwt.event.dom.client.BlurEvent;
+import com.google.gwt.event.dom.client.BlurHandler;
+import com.google.gwt.event.dom.client.FocusEvent;
+import com.google.gwt.event.dom.client.FocusHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.ui.TextBox;
+
+/**
+ * @author: Terry Chia (terrycwk1994@gmail.com)
+ */
+public class WatermarkedTextBox extends TextBox implements BlurHandler, FocusHandler {
+
+    private String watermark;
+    private HandlerRegistration blurHandler;
+    private HandlerRegistration focusHandler;
+
+    public WatermarkedTextBox() {
+        super();
+    }
+
+    public WatermarkedTextBox(String defaultValue) {
+        this();
+        setText(defaultValue);
+    }
+
+    public WatermarkedTextBox(String defaultValue, String watermark) {
+        this(defaultValue);
+        setWatermark(watermark);
+    }
+
+    public void setWatermark(final String watermark) {
+        this.watermark = watermark;
+
+        if((watermark != null) && (!watermark.equals(""))) {
+            blurHandler = addBlurHandler(this);
+            focusHandler = addFocusHandler(this);
+            EnableWatermark();
+        }
+
+        else {
+            blurHandler.removeHandler();
+            focusHandler.removeHandler();
+        }
+    }
+
+    @Override
+    public void onBlur(BlurEvent event) {
+        EnableWatermark();
+    }
+
+    @Override
+    public void onFocus(FocusEvent event) {
+        if(getText().equalsIgnoreCase(watermark)) {
+            setText("");
+            this.removeStyleName("watermarkInput");
+        }
+    }
+
+    private void EnableWatermark() {
+        String text = getText();
+        if ((text.length() == 0) || (text.equalsIgnoreCase(watermark)))
+        {
+            this.addStyleName("watermarkInput");
+            setText(watermark);
+        }
+    }
+}
