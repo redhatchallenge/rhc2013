274d914a5c1ba13928e58dfa61c31ffabb846df8
Ayrx <terrycwk1994@gmail.com>
2013-08-13 10:50:13 +0800

    Modified module path to reflect move to organization repo.

---
+++ src/main/java/org/redhatchallenge/rhc2013/shared/FieldVerifier.java
@@ -0,0 +1,23 @@
+package org.redhatchallenge.rhc2013.shared;
+
+/**
+ * @author: Terry Chia (terrycwk1994@gmail.com)
+ */
+public class FieldVerifier {
+
+    public static boolean isValidPassword(String password) {
+
+        // Password must be at least 8 characters and must contain 1 upper
+        // and 1 lower character.
+
+        return password.matches("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}");
+    }
+
+    public static boolean isValidEmail(String email) {
+        return email.toUpperCase().matches("^[0-9A-Z._-]+@[0-9A-Z]+.[0-9A-Z]{2,4}$");
+    }
+
+    public static boolean isValidContact(String contact) {
+        return contact.matches("^[0-9]+$");
+    }
+}
